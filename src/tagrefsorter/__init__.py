7c7c172a2c08b08ffbc8df733dae418f0d401e65
SuzuSys <sigmatics@outlook.jp>
2026-01-08 18:37:46 +0900

    fix: make versioning easier with git tags

--- src/tagrefsorter/__init__.py
+++ src/tagrefsorter/__init__.py
@@ -1,1 +1,7 @@
-__version__ = "0.1.4"
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("tagrefsorter")
+except PackageNotFoundError:
+    # package is not installed
+    pass
