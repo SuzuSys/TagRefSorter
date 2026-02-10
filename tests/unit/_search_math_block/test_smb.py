24a866bf8f7d8bd7ef15ae09b228301886c4a08c
SuzuSys <sigmatics@outlook.jp>
2026-02-10 23:22:02 +0900

    feat: add test files of _search_math_block

---
+++ tests/unit/_search_math_block/test_smb.py
@@ -0,0 +1,7 @@
+from tagrefsorter import parser
+
+def test_smb(smb_case):
+    tag_renumberer = parser.TagRenumberer()
+    for source, expected in smb_case:
+        math_blocks = tag_renumberer._search_math_block(source)
+        assert all(math_block == exp for math_block, exp in zip(math_blocks, expected))
