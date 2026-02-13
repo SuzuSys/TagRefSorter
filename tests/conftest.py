00c124b34a626e76410f1b2e0c5f1566a6f3548d
SuzuSys <sigmatics@outlook.jp>
2026-02-13 12:19:54 +0900

    feat: add latex_context fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from markdown_it import MarkdownIt
 from mdit_py_plugins.texmath import texmath_plugin
+from pylatexenc.macrospec import LatexContextDb, MacroSpec
+from pylatexenc.macrospec._argparsers import MacroStandardArgsParser
 
 
 @pytest.fixture(scope="session")
@@ -9,3 +11,11 @@ def md_parser() -> MarkdownIt:
     md = MarkdownIt().use(texmath_plugin)
     md.block.ruler.disable("math_block_eqno")
     return md
+
+
+@pytest.fixture(scope="session")
+def latex_context() -> LatexContextDb:
+    tag_spec = MacroSpec("tag", MacroStandardArgsParser("*{"))
+    latex_context = LatexContextDb()
+    latex_context.add_context_category(None, macros=[tag_spec], prepend=True)
+    return latex_context
